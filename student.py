5aea2eb34795791fe41a2a15d40412bf2ccfb43f
DanilleH22 <>
2023-12-25 22:49:19 +0000

    created test method to extend students end date

--- student.py
+++ student.py
@@ -5,19 +5,22 @@ class Student:
     """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.start_date = date.today()
-        self.enf_date = date.today() + timedelta(days=365)
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
 
     @property
     def full_name(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self._first_name} {self._last_name}"
 
     @property
     def email(self):
-        return f"{self.first_name.lower()}.{self.last_name.lower()}@email.com"
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
     def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
